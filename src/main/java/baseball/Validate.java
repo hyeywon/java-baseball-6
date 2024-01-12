ffddc9f8f11fc0bec71724d2ad14cecc363e1c5c
Hyewon Seok <hws2008@naver.com>
2024-01-12 10:34:33 +0900

    Feat: 입력값 유효성 검사 기능 추가

--- src/main/java/baseball/Validate.java
+++ src/main/java/baseball/Validate.java
@@ -1,16 +1,32 @@
 package baseball;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Validate {
     public List<Integer> answer(String input) {
-        // TODO
-        return new ArrayList<>();
+        try {
+            if (input.trim().length() != 3) {
+                throw new NumberFormatException();
+            }
+            return Arrays.stream(input.split(""))
+                    .mapToInt(Integer::parseInt)
+                    .boxed().collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값 입력");
+        }
     }
 
     public int restartOrFinish(String input) {
-        // TODO
-        return 0;
+        try {
+            int value = Integer.parseInt(input);
+            if ((value != 1) && (value != 2)) {
+                throw new NumberFormatException();
+            }
+            return value;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값 입력");
+        }
     }
 }
